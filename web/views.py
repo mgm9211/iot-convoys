eeb71e8d50d32b988faa15c79686689151f958d8
María González Moreno <mgm9211@gmail.com>
2021-06-19 12:37:19 +0200

    adapted templates

--- web/views.py
+++ web/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context = {}
+
+    context['username'] = request.session.get('username')
+    return render(request, "dashboard.html", context)
